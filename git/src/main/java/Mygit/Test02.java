94d544d7b402391bb65e910600af0b251d493167
hzh <hzh@qq.com>
2021-08-23 21:23:10 +0800

    hot-fix commit

--- git/src/main/java/Mygit/Test02.java
+++ git/src/main/java/Mygit/Test02.java
@@ -4,5 +4,6 @@ public class Test02 {
     public static void main(String[] args) {
         System.out.println("ncvt");
         System.out.println("666");
+        System.out.println("合并");
     }
 }
